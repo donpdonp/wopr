cb2b71ca572e9d1ba7448c8fef329db8bc34ad5e
Don Park <don@donpark.org>
2012-11-08 11:09:15 -0800

    akka hello world

--- src/main/scala/org/donpark/wopr/Daemon.scala
+++ src/main/scala/org/donpark/wopr/Daemon.scala
@@ -2,12 +2,15 @@ package org.donpark.wopr
 
 import org.json4s._
 import org.json4s.native.JsonMethods._
+import akka.actor.ActorSystem
 
 object Daemon {
   def main(args: Array[String]) {
 
-    val configFile = io.Source.fromFile("config.json").mkString
+    val configFile = io.Source.fromFile("config/settings.json").mkString
     val config = parse(configFile)
+
+    val system = ActorSystem("WOPR")
   }
 }
 
