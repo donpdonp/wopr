71863cd6eaa68ccf1786e597a5eb438d42d4bdd7
Don Park <don@donpark.org>
2012-11-12 15:05:29 -0800

    cli setup

--- lib/wopr/cli.rb
+++ lib/wopr/cli.rb
@@ -1,1 +1,9 @@
+require 'bundler/setup'
 require 'bluepill'
+require 'json'
+
+BASE_DIR = File.expand_path(File.dirname(__FILE__))+"/../../"
+SETTINGS = JSON.load(File.open(File.join(BASE_DIR,"config/settings.json")))
+
+opts = {:base_dir => File.join(BASE_DIR,".bluepill")}
+bluepill = Bluepill::Controller.new(opts)
