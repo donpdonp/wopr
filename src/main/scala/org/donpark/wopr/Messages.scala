91291ad9399b2c4c57895370ddad67f439143085
Don Park <don@donpark.org>
2012-11-12 14:51:12 -0800

    clear the room for a ruby version

--- src/main/scala/org/donpark/wopr/Messages.scala
+++
@@ -1,3 +0,0 @@
-package org.donpark.wopr
-
-case class ExchangeBalance
--- src/main/scala/org/donpark/wopr/models/Snapshot.scala
+++
@@ -1,8 +0,0 @@
-package org.donpark.wopr.models
-
-import org.squeryl.Schema
-import org.squeryl.annotations.Column
-import java.sql.Timestamp
-
-class Snapshot() extends Base with java.io.Serializable {
-}
