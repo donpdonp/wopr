f4f8a4ce50403fd1f9c2b7d0b4f0c8979ad7d377
Don Park <don@donpark.org>
2012-11-27 14:30:48 -0800

    split Wopr::Web

---
+++ lib/wopr/web.rb
@@ -0,0 +1,62 @@
+module Wopr
+  class Web
+    include Celluloid::IO
+
+    def websocket_mainloop
+      @clients = {}
+      puts "Socket 2000 listening"
+      server = TCPServer.new 'localhost', 2000
+      loop do
+        handle_connection! server.accept
+      end
+    end
+
+    def handle_connection(client)
+      client_addr = client.peeraddr(:numeric)
+      client_id = "#{client_addr[3]}:#{client_addr[1]}"
+      puts "Socket 2000 client #{client_id} #{@clients.size}"
+      handshake = WebSocket::Handshake::Server.new
+      begin
+        until handshake.finished?
+          msg = client.readpartial(4096)
+          handshake << msg
+        end
+        puts "handshake valid: #{handshake.valid?}"
+        if handshake.valid?
+          @clients.update(client_id => {socket: client,
+                                        ws_version: handshake.version})
+          puts "Responding to handshake"
+          client.write handshake.to_s
+          loop { read_frame(client_id) }
+        end
+
+      rescue EOFError
+        @clients.delete(client_id)
+        puts "client EOF"
+      end
+    end
+
+    def read_frame(client_id)
+      client = @clients[client_id]
+      frame = WebSocket::Frame::Incoming::Server.new(:version => client[:ws_version])
+      loop do
+        data = client[:socket].readpartial(4096)
+        frame << data
+        msg = frame.next
+        if msg
+          puts "#{client_id} MSG #{msg.type}#{msg.type == :text ? ": #{msg}" : "."}"
+        end
+      end
+    end
+
+    def send_all(json)
+      @clients.each do |client_id, client|
+        puts "Sending to #{client_id}"
+        out_frame = WebSocket::Frame::Outgoing::Server.new(:version => client[:ws_version],
+                                                           :data => json,
+                                                           :type => :text)
+        client[:socket].write out_frame.to_s
+      end
+    end
+  end
+end
