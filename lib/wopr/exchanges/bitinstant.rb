4cbd3ed4eacd8fe196a1ead93f1c156d9040d285
Don Park <don@donpark.org>
2013-03-18 13:43:32 -0700

    move the code into the core

--- lib/wopr/exchanges/bitinstant.rb
+++ lib/wopr/exchanges/bitinstant.rb
@@ -1,13 +1,53 @@
-require 'wopr/exchange_actor'
+require 'httparty'
+require 'json'
+require 'xmlrpc/client'
 
 module Wopr
   module Exchanges
     module Bitinstant
       class Rest
-        @rest_url = "https://www.bitinstant.com/api/json/"
-        @rpc_url = "https://www.bitinstant.com/api/xmlrpc"
-        @rpc = XMLRPC::Client.new_from_uri @rpc_url
+        @@rest_url = "https://www.bitinstant.com/api/json/"
 
+        def self.rest_post(path, params)
+          puts params.to_json
+          resp = HTTParty.post @@rest_url+path, :format => :json, :body => params.to_json,
+                                    :headers => { 'Content-Type' => 'application/json' }
+          resp.parsed_response
+        end
+
+        def self.rest_get(path, ordered_keys, params)
+          puts params.to_json
+          ordered_keys.each{|k| path += "/#{params[k]}"}
+          resp = HTTParty.get @@rest_url+path, :format => :json
+          resp.parsed_response
+        end
+
+        def self.fee(pay_method, dest_exchange, amount)
+          params = {:pay_method => pay_method,
+                    :dest_exchange => dest_exchange,
+                    :amount => amount,
+                    :currency => 'usd'}
+          ordered_keys = [:pay_method, :dest_exchange, :amount, :currency]
+          resp = rest_get('CalculateFee', ordered_keys, params)
+        end
+
+        def self.quote(pay_method, amount, dest_exchange, dest_account, email)
+          #params = [pay_method, amount, dest_exchange, dest_account]
+          #resp = @rpc.call('GetQuote', *params)
+          params = { :pay_method => pay_method,
+                     :amount => amount,
+                     :dest_exchange => dest_exchange,
+                     :dest_account => dest_account,
+                     :notify_email => email }
+          resp = rest_post('GetQuote', params)
+          puts resp.inspect
+        end
+
+      end
+
+      class XmlRpc
+        @@rpc_url = "https://www.bitinstant.com/api/xmlrpc"
+        @@rpc = XMLRPC::Client.new_from_uri @@rpc_url
       end
     end
   end
